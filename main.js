a7328bb16d7cb5359d7917c0ab334b68fe708e3a
Taylor McArthur <taydm.erek@gmail.com>
2017-03-15 21:52:44 -0700

    finished

--- main.js
+++ main.js
@@ -5,24 +5,26 @@ $(document).ready(function(){
 		var hours = currentTime.getHours();
 		var minutes = currentTime.getMinutes();
 		var seconds = currentTime.getSeconds();
+			if (seconds < 10) {
+				seconds = "0" + seconds;
+			}
+			if (hours > 12) {
+				hours = hours - 12;
+			}else if (hours < 10) {
+				hours = "0" + hours;
+			}
+			if (minutes < 10) {
+				minutes = "0" + minutes;
+			}
 
-		var clockDiv = document.getElementByID('clock');
+		var clockDiv = document.getElementById('clock');
 
-		clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " ";
+		clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
 
 	}
 
 	displayTime();
 	setInterval(displayTime, 1000);
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
 
 
 });
